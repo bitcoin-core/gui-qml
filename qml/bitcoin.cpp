01398aebabfeaba87be96f477f6a618123bf0ebc
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-08-27 16:54:11 +0300

    build, qml: Add qtquickcontrols2 module

--- qml/bitcoin.cpp
+++ qml/bitcoin.cpp
@@ -41,6 +41,8 @@ Q_IMPORT_PLUGIN(QtQmlPlugin)
 Q_IMPORT_PLUGIN(QtQmlModelsPlugin)
 Q_IMPORT_PLUGIN(QtQuick2Plugin)
 Q_IMPORT_PLUGIN(QtQuick2WindowPlugin)
+Q_IMPORT_PLUGIN(QtQuickControls2Plugin);
+Q_IMPORT_PLUGIN(QtQuickTemplates2Plugin);
 #endif
 
 namespace {
