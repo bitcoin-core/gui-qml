9a5a170f0528279313663de3e1ccd21367612563
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-10-02 22:13:33 +0300

    qml: Add qInstallMessageHandler
    Github-Pull: bitcoin-core/gui-qml#44
    Rebased-From: 6521cb8f118535bacc4d1d291a97c6d2109855d1

--- src/qml/BitcoinApp/bitcoin.cpp
+++ src/qml/BitcoinApp/bitcoin.cpp
@@ -10,6 +10,7 @@
 #include <init.h>
 #include <interfaces/node.h>
 #include <interfaces/init.h>
+#include <logging.h>
 #include <node/interface_ui.h>
 #include <node/context.h>
 #include <noui.h>
@@ -29,6 +30,10 @@
 #include <QQuickWindow>
 #include <QString>
 #include <QUrl>
+
+QT_BEGIN_NAMESPACE
+class QMessageLogContext;
+QT_END_NAMESPACE
 
 namespace {
 void SetupUIArgs(ArgsManager& argsman)
@@ -51,6 +56,17 @@ bool InitErrorMessageBox(
     }
     qGuiApp->exec();
     return false;
+}
+
+/* qDebug() message handler --> debug.log */
+void BitcoinQmlDebugMessageHandler(QtMsgType type, const QMessageLogContext& context, const QString& msg)
+{
+    Q_UNUSED(context);
+    if (type == QtDebugMsg) {
+        LogDebug(BCLog::QT, "GUI: %s\n", msg.toStdString());
+    } else {
+        LogInfo("GUI: %s\n", msg.toStdString());
+    }
 }
 } // namespace
 
@@ -133,6 +149,9 @@ int QmlGuiMain(int argc, char* argv[])
     if (!window) {
         return EXIT_FAILURE;
     }
+
+    // Install qDebug() message handler to route to debug.log
+    qInstallMessageHandler(BitcoinQmlDebugMessageHandler);
 
     qInfo() << "Graphics API in use:" << QmlUtil::GraphicsApi(window);
 
