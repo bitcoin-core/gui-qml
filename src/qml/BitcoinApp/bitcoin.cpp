3887f6d776030e61d4a1399af487a97bb4176ca4
João Barbosa <joao.paulo.barbosa@gmail.com>
2021-10-21 23:20:03 +0100

    qml: Set tab focus behavior
    Github-Pull: bitcoin-core/gui-qml#71
    Rebased-From: 500a6d20be4a5993c7c33bc613f841eca2233642

--- src/qml/BitcoinApp/bitcoin.cpp
+++ src/qml/BitcoinApp/bitcoin.cpp
@@ -34,6 +34,7 @@
 #include <QQmlContext>
 #include <QQuickWindow>
 #include <QString>
+#include <QStyleHints>
 #include <QUrl>
 
 QT_BEGIN_NAMESPACE
@@ -84,6 +85,7 @@ int QmlGuiMain(int argc, char* argv[])
     std::tie(argc, argv) = winArgs.get();
 #endif
 
+    QGuiApplication::styleHints()->setTabFocusBehavior(Qt::TabFocusAllControls);
     QGuiApplication app(argc, argv);
 
     auto handler_message_box = ::uiInterface.ThreadSafeMessageBox_connect(InitErrorMessageBox);
