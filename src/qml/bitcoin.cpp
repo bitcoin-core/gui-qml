4b59e56024ee506db877407a62c4fbffdbf6b794
João Barbosa <joao.paulo.barbosa@gmail.com>
2021-11-03 17:43:08 +0000

    ci: Set alias in tasks with artifacts in cirrus.yml
    Rebased-From: 011fcfff722982e20fc9a7ba39d276e71d8e585e
    Github-Pull: bitcoin-core/gui-qml#79

--- src/qml/bitcoin.cpp
+++ src/qml/bitcoin.cpp
@@ -42,10 +42,8 @@ QT_END_NAMESPACE
 
 #if defined(QT_STATICPLUGIN)
 #include <QtPlugin>
-Q_IMPORT_PLUGIN(QtQuick2DialogsPlugin);
 Q_IMPORT_PLUGIN(QtQuick2Plugin);
 Q_IMPORT_PLUGIN(QtQuick2WindowPlugin);
-Q_IMPORT_PLUGIN(QtQuickControls1Plugin);
 Q_IMPORT_PLUGIN(QtQuickControls2Plugin);
 Q_IMPORT_PLUGIN(QtQuickLayoutsPlugin);
 Q_IMPORT_PLUGIN(QtQuickTemplates2Plugin);
