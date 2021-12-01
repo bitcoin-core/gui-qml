081f618919b65ef64caa1ab8fc8018f71d20213f
Jarol Rodriguez <jarolrod@tutanota.com>
2021-11-30 19:29:04 -0500

    qml: drop obsolete debug message handler
    Filtered staging history keeps this path-level compatibility change.
    
    Rebased-From: 28ae857569bb490f8b2272c56c4e08e032aefc03
    Github-Pull: bitcoin-core/gui-qml#87

--- src/qml/bitcoin.cpp
+++ src/qml/bitcoin.cpp
@@ -8,7 +8,7 @@
 #include <init.h>
 #include <interfaces/init.h>
 #include <interfaces/node.h>
-#include <logging.h>
+#include <node/context.h>
 #include <node/ui_interface.h>
 #include <noui.h>
 #include <qml/imageprovider.h>
@@ -32,12 +32,8 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickWindow>
-#include <QString>
+#include <QStringLiteral>
 #include <QUrl>
-
-QT_BEGIN_NAMESPACE
-class QMessageLogContext;
-QT_END_NAMESPACE
 
 #if defined(QT_STATICPLUGIN)
 #include <QtPlugin>
@@ -46,7 +42,6 @@ Q_IMPORT_PLUGIN(QtQuick2Plugin);
 Q_IMPORT_PLUGIN(QtQuick2WindowPlugin);
 Q_IMPORT_PLUGIN(QtQuickControls1Plugin);
 Q_IMPORT_PLUGIN(QtQuickControls2Plugin);
-Q_IMPORT_PLUGIN(QtQuickLayoutsPlugin);
 Q_IMPORT_PLUGIN(QtQuickTemplates2Plugin);
 #endif
 
@@ -73,17 +68,6 @@ bool InitErrorMessageBox(
     qGuiApp->exec();
     return false;
 }
-
-/* qDebug() message handler --> debug.log */
-void DebugMessageHandler(QtMsgType type, const QMessageLogContext& context, const QString& msg)
-{
-    Q_UNUSED(context);
-    if (type == QtDebugMsg) {
-        LogPrint(BCLog::QT, "GUI: %s\n", msg.toStdString());
-    } else {
-        LogPrintf("GUI: %s\n", msg.toStdString());
-    }
-}
 } // namespace
 
 
@@ -101,12 +85,15 @@ int QmlGuiMain(int argc, char* argv[])
 
     auto handler_message_box = ::uiInterface.ThreadSafeMessageBox_connect(InitErrorMessageBox);
 
-    std::unique_ptr<interfaces::Init> init = interfaces::MakeGuiInit(argc, argv);
+    NodeContext node_context;
+    int unused_exit_status;
+    std::unique_ptr<interfaces::Init> init = interfaces::MakeNodeInit(node_context, argc, argv, unused_exit_status);
 
     SetupEnvironment();
     util::ThreadSetInternalName("main");
 
     /// Parse command-line options. We do this after qt in order to show an error if there are problems parsing these.
+    node_context.args = &gArgs;
     SetupServerArgs(gArgs);
     SetupUIArgs(gArgs);
     std::string error;
@@ -191,9 +178,6 @@ int QmlGuiMain(int argc, char* argv[])
     if (!window) {
         return EXIT_FAILURE;
     }
-
-    // Install qDebug() message handler to route to debug.log
-    qInstallMessageHandler(DebugMessageHandler);
 
     qInfo() << "Graphics API in use:" << QmlUtil::GraphicsApi(window);
 
--- src/qml/pages/stub.qml
+++ src/qml/pages/stub.qml
@@ -4,7 +4,6 @@
 
 import QtQuick 2.12
 import QtQuick.Controls 2.12
-import QtQuick.Layouts 1.11
 import "../components" as BitcoinCoreComponents
 
 
