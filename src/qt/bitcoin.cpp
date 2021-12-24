fea00df309be1415f51523a12eec72290ee7ae2e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-12-24 11:41:09 +0200

    Revert "qt: Move static QPA plugin imports into main.cpp"
    This reverts commit b1b630476056159873134341885d960fe2856518.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -57,9 +57,13 @@
 
 #if defined(QT_STATICPLUGIN)
 #include <QtPlugin>
-#if defined(QT_QPA_PLATFORM_WINDOWS)
+#if defined(QT_QPA_PLATFORM_XCB)
+Q_IMPORT_PLUGIN(QXcbIntegrationPlugin);
+#elif defined(QT_QPA_PLATFORM_WINDOWS)
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
 Q_IMPORT_PLUGIN(QWindowsVistaStylePlugin);
 #elif defined(QT_QPA_PLATFORM_COCOA)
+Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
 Q_IMPORT_PLUGIN(QMacStylePlugin);
 #endif
 #endif
