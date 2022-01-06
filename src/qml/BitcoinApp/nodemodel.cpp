d47ab9a2c8ac9f808850b7f57d1713487fc79ad1
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-01-06 15:14:26 +0200

    qml: Add shutdown polling facilities to the NodeModel class
    Github-Pull: bitcoin-core/gui-qml#108
    Rebased-From: b6c01bbd55803710a5be469a5b06ac631cedcb66

--- src/qml/BitcoinApp/nodemodel.cpp
+++ src/qml/BitcoinApp/nodemodel.cpp
@@ -8,6 +8,9 @@
 #include <validation.h>
 
 #include <cassert>
+#include <chrono>
+
+#include <QTimerEvent>
 
 NodeModel::NodeModel(interfaces::Node& node)
     : m_node{node}
@@ -46,6 +49,25 @@ void NodeModel::initializeResult([[maybe_unused]] bool success, interfaces::Bloc
     // TODO: Handle the `success` parameter,
     setBlockTipHeight(tip_info.block_height);
     setVerificationProgress(tip_info.verification_progress);
+}
+
+void NodeModel::startShutdownPolling()
+{
+    m_shutdown_polling_timer_id = startTimer(200ms);
+}
+
+void NodeModel::stopShutdownPolling()
+{
+    killTimer(m_shutdown_polling_timer_id);
+}
+
+void NodeModel::timerEvent(QTimerEvent* event)
+{
+    Q_UNUSED(event)
+    if (m_node.shutdownRequested()) {
+        stopShutdownPolling();
+        Q_EMIT requestedShutdown();
+    }
 }
 
 void NodeModel::ConnectToBlockTipSignal()
