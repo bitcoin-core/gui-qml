d740e90dfbab161286ad7768a1511ee3fa198420
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-01-09 19:07:41 +0200

    qml: remove obsolete debug message handler
    Filtered staging history keeps this path-level compatibility change.
    
    Rebased-From: 47de82740f0e3f75beb2fd9fe89b7125f1b86f62
    Github-Pull: bitcoin-core/gui-qml#110

--- src/qml/bitcoin.cpp
+++ src/qml/bitcoin.cpp
@@ -8,7 +8,6 @@
 #include <init.h>
 #include <interfaces/init.h>
 #include <interfaces/node.h>
-#include <logging.h>
 #include <node/context.h>
 #include <node/ui_interface.h>
 #include <noui.h>
@@ -33,13 +32,8 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickWindow>
-#include <QString>
 #include <QStringLiteral>
 #include <QUrl>
-
-QT_BEGIN_NAMESPACE
-class QMessageLogContext;
-QT_END_NAMESPACE
 
 #if defined(QT_STATICPLUGIN)
 #include <QtPlugin>
@@ -73,17 +67,6 @@ bool InitErrorMessageBox(
     }
     qGuiApp->exec();
     return false;
-}
-
-/* qDebug() message handler --> debug.log */
-void DebugMessageHandler(QtMsgType type, const QMessageLogContext& context, const QString& msg)
-{
-    Q_UNUSED(context);
-    if (type == QtDebugMsg) {
-        LogPrint(BCLog::QT, "GUI: %s\n", msg.toStdString());
-    } else {
-        LogPrintf("GUI: %s\n", msg.toStdString());
-    }
 }
 } // namespace
 
@@ -194,9 +177,6 @@ int QmlGuiMain(int argc, char* argv[])
     if (!window) {
         return EXIT_FAILURE;
     }
-
-    // Install qDebug() message handler to route to debug.log
-    qInstallMessageHandler(DebugMessageHandler);
 
     qInfo() << "Graphics API in use:" << QmlUtil::GraphicsApi(window);
 
