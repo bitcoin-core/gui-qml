556c4f16658485c9afac505b880eab6d21a68511
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-05-26 20:09:50 +0200

    Revert "qml: Set tab focus behavior"
    This reverts commit c7647c53b117d024fba92a78dbd0db081008e974.

--- src/qml/bitcoin.cpp
+++ src/qml/bitcoin.cpp
@@ -34,7 +34,6 @@
 #include <QQmlContext>
 #include <QQuickWindow>
 #include <QString>
-#include <QStyleHints>
 #include <QUrl>
 
 QT_BEGIN_NAMESPACE
@@ -99,7 +98,6 @@ int QmlGuiMain(int argc, char* argv[])
     Q_INIT_RESOURCE(bitcoin_qml);
 
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication::styleHints()->setTabFocusBehavior(Qt::TabFocusAllControls);
     QGuiApplication app(argc, argv);
 
     auto handler_message_box = ::uiInterface.ThreadSafeMessageBox_connect(InitErrorMessageBox);
