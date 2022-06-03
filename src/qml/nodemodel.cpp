3c0773ca3c604dac03c98150fa46dcda47d2437d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-06-03 19:23:50 +0200

    qml: Add `NodeModel::numOutboundPeers` property

--- src/qml/nodemodel.cpp
+++ src/qml/nodemodel.cpp
@@ -5,6 +5,8 @@
 #include <qml/nodemodel.h>
 
 #include <interfaces/node.h>
+#include <net.h>
+#include <node/interface_ui.h>
 #include <validation.h>
 
 #include <cassert>
@@ -18,6 +20,7 @@ NodeModel::NodeModel(interfaces::Node& node)
     : m_node{node}
 {
     ConnectToBlockTipSignal();
+    ConnectToNumConnectionsChangedSignal();
 }
 
 void NodeModel::setBlockTipHeight(int new_height)
@@ -25,6 +28,14 @@ void NodeModel::setBlockTipHeight(int new_height)
     if (new_height != m_block_tip_height) {
         m_block_tip_height = new_height;
         Q_EMIT blockTipHeightChanged();
+    }
+}
+
+void NodeModel::setNumOutboundPeers(int new_num)
+{
+    if (new_num != m_num_outbound_peers) {
+        m_num_outbound_peers = new_num;
+        Q_EMIT numOutboundPeersChanged();
     }
 }
 
@@ -127,5 +138,15 @@ void NodeModel::ConnectToBlockTipSignal()
 
                 Q_EMIT setTimeRatioList(tip.block_time);
             });
+        });
+}
+
+void NodeModel::ConnectToNumConnectionsChangedSignal()
+{
+    assert(!m_handler_notify_num_peers_changed);
+
+    m_handler_notify_num_peers_changed = m_node.handleNotifyNumConnectionsChanged(
+        [this](PeersNumByType new_num_peers) {
+            setNumOutboundPeers(new_num_peers.outbound_full_relay + new_num_peers.block_relay);
         });
 }
--- src/qml/nodemodel.h
+++ src/qml/nodemodel.h
@@ -25,6 +25,8 @@ class NodeModel : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(int blockTipHeight READ blockTipHeight NOTIFY blockTipHeightChanged)
+    Q_PROPERTY(int numOutboundPeers READ numOutboundPeers NOTIFY numOutboundPeersChanged)
+    Q_PROPERTY(int maxNumOutboundPeers READ maxNumOutboundPeers CONSTANT)
     Q_PROPERTY(int remainingSyncTime READ remainingSyncTime NOTIFY remainingSyncTimeChanged)
     Q_PROPERTY(double verificationProgress READ verificationProgress NOTIFY verificationProgressChanged)
     Q_PROPERTY(bool pause READ pause WRITE setPause NOTIFY pauseChanged)
@@ -34,6 +36,9 @@ public:
 
     int blockTipHeight() const { return m_block_tip_height; }
     void setBlockTipHeight(int new_height);
+    int numOutboundPeers() const { return m_num_outbound_peers; }
+    void setNumOutboundPeers(int new_num);
+    int maxNumOutboundPeers() const { return m_max_num_outbound_peers; }
     int remainingSyncTime() const { return m_remaining_sync_time; }
     void setRemainingSyncTime(double new_progress);
     double verificationProgress() const { return m_verification_progress; }
@@ -51,6 +56,7 @@ public Q_SLOTS:
 
 Q_SIGNALS:
     void blockTipHeightChanged();
+    void numOutboundPeersChanged();
     void remainingSyncTimeChanged();
     void requestedInitialize();
     void requestedShutdown();
@@ -66,6 +72,8 @@ protected:
 private:
     // Properties that are exposed to QML.
     int m_block_tip_height{0};
+    int m_num_outbound_peers{0};
+    static constexpr int m_max_num_outbound_peers{MAX_OUTBOUND_FULL_RELAY_CONNECTIONS + MAX_BLOCK_RELAY_ONLY_CONNECTIONS};
     int m_remaining_sync_time{0};
     double m_verification_progress{0.0};
     bool m_pause{false};
@@ -76,8 +84,10 @@ private:
 
     interfaces::Node& m_node;
     std::unique_ptr<interfaces::Handler> m_handler_notify_block_tip;
+    std::unique_ptr<interfaces::Handler> m_handler_notify_num_peers_changed;
 
     void ConnectToBlockTipSignal();
+    void ConnectToNumConnectionsChangedSignal();
 };
 
 #endif // BITCOIN_QML_NODEMODEL_H
