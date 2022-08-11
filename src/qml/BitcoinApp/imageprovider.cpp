7f78760dd6083612141a9954e05baf71ae6344a9
Jarol Rodriguez <jarolrod@tutanota.com>
2022-08-10 23:30:01 -0400

    qml: introduce arrow-up icon
    Github-Pull: bitcoin-core/gui-qml#151
    Rebased-From: 68d57200b440b18a3354125b8722bfeb89159d19

--- src/qml/BitcoinApp/imageprovider.cpp
+++ src/qml/BitcoinApp/imageprovider.cpp
@@ -22,6 +22,11 @@ QPixmap ImageProvider::requestPixmap(const QString& id, QSize* size, const QSize
         return {};
     }
 
+    if (id == "arrow-up") {
+        *size = requested_size;
+        return QIcon(":/qt/qml/BitcoinApp/res/icons/arrow-up.png").pixmap(requested_size);
+    }
+
     if (id == "app") {
         *size = requested_size;
         return m_network_style->getAppIcon().pixmap(requested_size);
