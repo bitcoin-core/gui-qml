84a9a1022f6ddba49cc958c3b65b841b933eee26
Jarol Rodriguez <jarolrod@tutanota.com>
2022-08-14 01:16:06 -0400

    qml: introduce static backup for lottie blocktime element
    Rebased-From: 41bb56ce73308848035aecbcacbe57497f7afa37
    Github-Pull: bitcoin-core/gui-qml#156

--- src/qml/imageprovider.cpp
+++ src/qml/imageprovider.cpp
@@ -32,6 +32,16 @@ QPixmap ImageProvider::requestPixmap(const QString& id, QSize* size, const QSize
         return QIcon(":/icons/arrow-up").pixmap(requested_size);
     }
 
+    if (id == "blocktime-dark") {
+        *size = requested_size;
+        return QIcon(":/icons/blocktime-dark").pixmap(requested_size);
+    }
+
+    if (id == "blocktime-light") {
+        *size = requested_size;
+        return QIcon(":/icons/blocktime-light").pixmap(requested_size);
+    }
+
     if (id == "app") {
         *size = requested_size;
         return m_network_style->getAppIcon().pixmap(requested_size);
@@ -50,6 +60,11 @@ QPixmap ImageProvider::requestPixmap(const QString& id, QSize* size, const QSize
     if (id == "export") {
         *size = requested_size;
         return QIcon(":/icons/export").pixmap(requested_size);
+    }
+
+    if (id == "gear") {
+        *size = requested_size;
+        return QIcon(":/icons/gear").pixmap(requested_size);
     }
 
     if (id == "info") {
