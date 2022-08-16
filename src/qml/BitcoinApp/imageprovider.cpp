5fdea2e540e76102f571d142aba213abfd68c894
Jarol Rodriguez <jarolrod@tutanota.com>
2022-08-15 21:39:54 -0400

    qml: introduce cross icon
    Github-Pull: bitcoin-core/gui-qml#162
    Rebased-From: 9848f2050d0a05dfca38d2485c0f49df84c21807

--- src/qml/BitcoinApp/imageprovider.cpp
+++ src/qml/BitcoinApp/imageprovider.cpp
@@ -57,6 +57,11 @@ QPixmap ImageProvider::requestPixmap(const QString& id, QSize* size, const QSize
         return QIcon(":/qt/qml/BitcoinApp/res/icons/caret-right.png").pixmap(requested_size);
     }
 
+    if (id == "cross") {
+        *size = requested_size;
+        return QIcon(":/qt/qml/BitcoinApp/res/icons/cross.png").pixmap(requested_size);
+    }
+
     if (id == "export") {
         *size = requested_size;
         return QIcon(":/qt/qml/BitcoinApp/res/icons/export.png").pixmap(requested_size);
