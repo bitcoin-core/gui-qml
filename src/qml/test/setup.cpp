ad9038f8801231e5b94938f54883c3aacecc4bbe
Johnny Carlson <JohnCarlson@pm.me>
2023-01-14 14:19:51 -0500

    qmltest: rename onboardingtests to setup

--- src/qml/test/setup.cpp
+++ src/qml/test/setup.cpp
@@ -1,15 +1,12 @@
 // Copyright (c) 2023 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#include <qml/test/setup.h>
+
 #include <QtQuickTest>
 #include <QQmlEngine>
 #include <QQmlContext>
 #include <qml/test/testimageprovider.h>
-#include <qml/test/onboardingtests.h>
-
-Setup::Setup()
-{
-}
 
 void Setup::qmlEngineAvailable(QQmlEngine * engine) {
     engine->addImageProvider(QStringLiteral("images"), new TestImageProvider());
--- src/qml/test/setup.h
+++ src/qml/test/setup.h
@@ -1,8 +1,8 @@
 // Copyright (c) 2014-2023 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_QML_TEST_ONBOARDINGTESTS_H
-#define BITCOIN_QML_TEST_ONBOARDINGTESTS_H
+#ifndef BITCOIN_QML_TEST_SETUP_H
+#define BITCOIN_QML_TEST_SETUP_H
 
 #include <QObject>
 #include <QQmlEngine>
@@ -12,11 +12,11 @@ class Setup : public QObject
     Q_OBJECT
 
 public:
-    Setup();
+    Setup() = default;
     ~Setup() = default;
 
 public Q_SLOTS:
     void qmlEngineAvailable(QQmlEngine * engine);
 };
 
-#endif // BITCOIN_QML_TEST_ONBOARDINGTESTS_H
+#endif // BITCOIN_QML_TEST_SETUP_H
