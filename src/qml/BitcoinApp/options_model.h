5eb106cb35f936a51fd41e8f90dbe19f8fa46499
jarolrod <jarolrod@tutanota.com>
2023-01-24 00:29:47 -0500

    qml: add and wire listen setting to OptionsModel backend
    Github-Pull: bitcoin-core/gui-qml#222
    Rebased-From: faeb551f77933de2a7b2abee81510c166328df83

--- src/qml/BitcoinApp/options_model.h
+++ src/qml/BitcoinApp/options_model.h
@@ -17,18 +17,22 @@ class Node;
 class OptionsQmlModel : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(bool listen READ listen WRITE setListen NOTIFY listenChanged)
     Q_PROPERTY(bool prune READ prune WRITE setPrune NOTIFY pruneChanged)
     Q_PROPERTY(int pruneSizeGB READ pruneSizeGB WRITE setPruneSizeGB NOTIFY pruneSizeGBChanged)
 
 public:
     explicit OptionsQmlModel(interfaces::Node& node);
 
+    bool listen() const { return m_listen; }
+    void setListen(bool new_listen);
     bool prune() const { return m_prune; }
     void setPrune(bool new_prune);
     int pruneSizeGB() const { return m_prune_size_gb; }
     void setPruneSizeGB(int new_prune_size);
 
 Q_SIGNALS:
+    void listenChanged(bool new_listen);
     void pruneChanged(bool new_prune);
     void pruneSizeGBChanged(int new_prune_size_gb);
 
@@ -36,6 +40,7 @@ private:
     interfaces::Node& m_node;
 
     // Properties that are exposed to QML.
+    bool m_listen;
     bool m_prune;
     int m_prune_size_gb;
 
