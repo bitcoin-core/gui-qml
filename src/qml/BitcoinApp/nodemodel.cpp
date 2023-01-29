3959586481e4ff9cf66e5ebfe3edf32625bed9a2
johnny9 <985648+johnny9@users.noreply.github.com>
2023-01-28 20:19:59 -0500

    qml: add ability to pause NodeModel
    Github-Pull: bitcoin-core/gui-qml#220
    Rebased-From: 094a2c43b53ab5f3efcf693d26ec3d9bbcc70261
    
    Co-authored-by: shaavan <shaavan.github@gmail.com>

--- src/qml/BitcoinApp/nodemodel.cpp
+++ src/qml/BitcoinApp/nodemodel.cpp
@@ -35,6 +35,15 @@ void NodeModel::setVerificationProgress(double new_progress)
     }
 }
 
+void NodeModel::setPause(bool new_pause)
+{
+    if(m_pause != new_pause) {
+        m_pause = new_pause;
+        m_node.setNetworkActive(!new_pause);
+        Q_EMIT pauseChanged(new_pause);
+    }
+}
+
 void NodeModel::startNodeInitializionThread()
 {
     Q_EMIT requestedInitialize();
