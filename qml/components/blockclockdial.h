564e45eb496f809d0935af5a09634462307e443d
jarolrod <jarolrod@tutanota.com>
2023-02-13 01:44:15 -0700

    qml: Add connection animation to block clock

--- qml/components/blockclockdial.h
+++ qml/components/blockclockdial.h
@@ -6,6 +6,7 @@
 #define BITCOIN_QML_COMPONENTS_BLOCKCLOCKDIAL_H
 
 #include <QQuickPaintedItem>
+#include <QConicalGradient>
 #include <QPainter>
 
 class BlockClockDial : public QQuickPaintedItem
@@ -13,6 +14,7 @@ class BlockClockDial : public QQuickPaintedItem
     Q_OBJECT
     Q_PROPERTY(QVariantList timeRatioList READ timeRatioList WRITE setTimeRatioList)
     Q_PROPERTY(double verificationProgress READ verificationProgress WRITE setVerificationProgress)
+    Q_PROPERTY(bool connected READ connected WRITE setConnected)
     Q_PROPERTY(bool synced READ synced WRITE setSynced)
     Q_PROPERTY(bool paused READ paused WRITE setPaused)
     Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor)
@@ -25,6 +27,7 @@ public:
 
     QVariantList timeRatioList() const { return m_time_ratio_list; };
     double verificationProgress() const { return m_verification_progress; };
+    bool connected() const { return m_is_connected; };
     bool synced() const { return m_is_synced; };
     bool paused() const { return m_is_paused; };
     QColor backgroundColor() const { return m_background_color; };
@@ -34,6 +37,7 @@ public:
 public Q_SLOTS:
     void setTimeRatioList(QVariantList new_time);
     void setVerificationProgress(double progress);
+    void setConnected(bool connected);
     void setSynced(bool synced);
     void setPaused(bool paused);
     void setBackgroundColor(QColor color);
@@ -41,18 +45,25 @@ public Q_SLOTS:
     void setTimeTickColor(QColor color);
 
 private:
+    void paintConnectingAnimation(QPainter * painter);
     void paintProgress(QPainter * painter);
     void paintBlocks(QPainter * painter);
     void paintBackground(QPainter * painter);
     void paintTimeTicks(QPainter * painter);
     QRectF getBoundsForPen(const QPen & pen);
     double degreesPerPixel();
+    void setupConnectingGradient(const QPen & pen);
+    qreal decrementGradientAngle(qreal angle);
 
     QVariantList m_time_ratio_list;
     double m_verification_progress;
+    bool m_is_connected;
     bool m_is_synced;
     bool m_is_paused;
     QColor m_background_color;
+    QConicalGradient m_connecting_gradient;
+    qreal m_connecting_start_angle = 90;
+    const qreal m_connecting_end_angle = -180;
     QList<QColor> m_confirmation_colors;
     QColor m_time_tick_color;
 };
