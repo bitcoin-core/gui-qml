5a019cc20e15cd7e3f0552ca7dced823ef711cc2
johnny9 <985648+johnny9@users.noreply.github.com>
2023-03-29 23:16:24 -0400

    android: put activity into fullscreen immersive mode

--- src/qt/android/src/org/bitcoincore/qt/BitcoinQtActivity.java
+++ src/qt/android/src/org/bitcoincore/qt/BitcoinQtActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.system.ErrnoException;
 import android.system.Os;
 import android.view.WindowManager;
+import android.view.View;
 
 import org.qtproject.qt5.android.bindings.QtActivity;
 
@@ -20,6 +21,10 @@ public class BitcoinQtActivity extends QtActivity
         }
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
     }
 }
