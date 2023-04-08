49e4bdbf5032b6f6bdbeb1d759c382b7a1893310
johnny9 <985648+johnny9@users.noreply.github.com>
2023-04-07 21:19:10 -0400

    android: obtain PowerManager PARTIAL_WAKE_LOCK
    Gives assurance that the CPU will continue to run our tasks while
    the screen is off.

--- src/qt/android/src/org/bitcoincore/qt/BitcoinQtService.java
+++ src/qt/android/src/org/bitcoincore/qt/BitcoinQtService.java
@@ -11,9 +11,12 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.util.Log;
 import org.qtproject.qt5.android.bindings.QtService;
+import android.content.Context;
+import android.os.PowerManager;
 
 public class BitcoinQtService extends QtService
 {
+    private PowerManager.WakeLock wakeLock;
 
     @Override
     public void onCreate() {
@@ -39,11 +42,22 @@ public class BitcoinQtService extends QtService
             .build();
 
         startForeground(1, notification);
+        PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "BitcoinCore::IBD");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
+        wakeLock.acquire();
         return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (wakeLock.isHeld()) {
+            wakeLock.release(); // Release the wake lock
+        }
     }
 }
