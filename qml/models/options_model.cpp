5f75487ba398ceb9fa90a7b374e8fb7b05c81fe3
jarolrod <jarolrod@tutanota.com>
2023-05-26 17:00:04 -0400

    qml: move backend models into models directory

--- qml/models/options_model.cpp
+++ qml/models/options_model.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <qml/options_model.h>
+#include <qml/models/options_model.h>
 
 #include <interfaces/node.h>
 #include <qt/guiconstants.h>
