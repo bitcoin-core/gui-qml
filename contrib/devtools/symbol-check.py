3aa9d03a4fee709eb1edf13a75dd6e3ae8589d28
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-08-29 12:17:48 +0100

    Revert "qml, build: Update PE_ALLOWED_LIBRARIES for Qt 5.15.2"
    This reverts commit c8f4e0398d0e45c0aaef0d98b7835c16080a07ea.

--- contrib/devtools/symbol-check.py
+++ contrib/devtools/symbol-check.py
@@ -155,8 +155,6 @@ PE_ALLOWED_LIBRARIES = {
 'VERSION.dll', # version checking
 'WINMM.dll', # WinMM audio API
 'WTSAPI32.dll',
-'d3d11.dll',
-'dxgi.dll',
 }
 
 def check_version(max_versions, version, arch) -> bool:
