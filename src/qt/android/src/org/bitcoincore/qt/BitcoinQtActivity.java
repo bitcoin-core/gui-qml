67608bd494d9cca9a95bc75ef80d5850ce879173
D33r-Gee <d33r.gee@gmail.com>
2024-07-02 11:48:30 -0700

    android: permissions for custom datadir

--- src/qt/android/src/org/bitcoincore/qt/BitcoinQtActivity.java
+++ src/qt/android/src/org/bitcoincore/qt/BitcoinQtActivity.java
@@ -8,6 +8,8 @@ import android.system.ErrnoException;
 import android.system.Os;
 import android.view.WindowManager;
 import android.view.View;
+import android.Manifest;
+import android.content.pm.PackageManager;
 
 import org.qtproject.qt5.android.bindings.QtActivity;
 
@@ -15,6 +17,8 @@ import java.io.File;
 
 public class BitcoinQtActivity extends QtActivity
 {
+    private static final int PERMISSIONS_REQUEST_CODE = 123;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -36,5 +40,22 @@ public class BitcoinQtActivity extends QtActivity
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
+
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                System.out.println("Permission was granted");
+            } else {
+                System.out.println("Permission was denied");
+            }
+        }
     }
 }
