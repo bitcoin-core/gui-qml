dddc53e850ed98385f5490f6faa271f28f139aa2
johnny9 <985648+johnny9@users.noreply.github.com>
2025-03-28 13:23:59 -0400

    qml: Introduce SendRecipientsListModel

---
+++ src/qml/models/sendrecipientslistmodel.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2025 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qml/models/sendrecipient.h>
+#include <qml/models/sendrecipientslistmodel.h>
+
+SendRecipientsListModel::SendRecipientsListModel(QObject* parent)
+    : QAbstractListModel(parent)
+{
+}
+
+int SendRecipientsListModel::rowCount(const QModelIndex&) const
+{
+    return m_recipients.size();
+}
+
+QVariant SendRecipientsListModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid() || index.row() >= m_recipients.size())
+        return {};
+
+    const auto& r = m_recipients[index.row()];
+    switch (role) {
+    case AddressRole: return r->address();
+    case LabelRole: return r->label();
+    case AmountRole: return r->amount();
+    case MessageRole: return r->message();
+    default: return {};
+    }
+    return {};
+}
+
+QHash<int, QByteArray> SendRecipientsListModel::roleNames() const
+{
+    return {
+        {AddressRole, "address"},
+        {LabelRole, "label"}};
+}
+
+void SendRecipientsListModel::addRecipient()
+{
+    beginInsertRows(QModelIndex(), m_recipients.size(), m_recipients.size());
+    m_recipients.append(new SendRecipient(this));
+    endInsertRows();
+}
---
+++ src/qml/models/sendrecipientslistmodel.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2025 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QML_MODELS_SENDRECIPIENTSLISTMODEL_H
+#define BITCOIN_QML_MODELS_SENDRECIPIENTSLISTMODEL_H
+
+#include <qml/models/sendrecipient.h>
+
+#include <QAbstractListModel>
+#include <QList>
+
+class SendRecipientsListModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+
+public:
+    enum Roles {
+        AddressRole = Qt::UserRole + 1,
+        LabelRole,
+        AmountRole,
+        MessageRole
+    };
+
+    explicit SendRecipientsListModel(QObject* parent = nullptr);
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    void addRecipient();
+
+private:
+    QList<SendRecipient*> m_recipients;
+};
+
+#endif // BITCOIN_QML_MODELS_SENDRECIPIENTSLISTMODEL_H
