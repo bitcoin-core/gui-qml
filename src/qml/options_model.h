peter97822/bitcoin

qml: Expose minimum and maximum valid Script Verif Threads values

src/qml/options_model.h
@@ -6,6 +6,8 @@
 #define BITCOIN_QML_OPTIONS_MODEL_H
 
 #include <util/settings.h>
+#include <util/system.h>
+#include <validation.h>
 
 #include <QObject>
 
@@ -19,6 +21,8 @@ class OptionsQmlModel : public QObject
     Q_OBJECT
     Q_PROPERTY(int dbcacheSizeMiB READ dbcacheSizeMiB WRITE setDbcacheSizeMiB NOTIFY dbcacheSizeMiBChanged)
     Q_PROPERTY(bool listen READ listen WRITE setListen NOTIFY listenChanged)
+    Q_PROPERTY(int maxScriptThreads READ maxScriptThreads CONSTANT)
+    Q_PROPERTY(int minScriptThreads READ minScriptThreads CONSTANT)
     Q_PROPERTY(bool natpmp READ natpmp WRITE setNatpmp NOTIFY natpmpChanged)
     Q_PROPERTY(bool prune READ prune WRITE setPrune NOTIFY pruneChanged)
     Q_PROPERTY(int pruneSizeGB READ pruneSizeGB WRITE setPruneSizeGB NOTIFY pruneSizeGBChanged)
@@ -33,6 +37,8 @@ public:
     void setDbcacheSizeMiB(int new_dbcache_size_mib);
     bool listen() const { return m_listen; }
     void setListen(bool new_listen);
+    int maxScriptThreads() const { return m_max_script_threads; }
+    int minScriptThreads() const { return m_min_script_threads; }
     bool natpmp() const { return m_natpmp; }
     void setNatpmp(bool new_natpmp);
     bool prune() const { return m_prune; }
@@ -62,6 +68,8 @@ private:
     // Properties that are exposed to QML.
     int m_dbcache_size_mib;
     bool m_listen;
+    const int m_max_script_threads{MAX_SCRIPTCHECK_THREADS};
+    const int m_min_script_threads{-GetNumCores()};
     bool m_natpmp;
     bool m_prune;
     int m_prune_size_gb;

bitcoin-core/gui-qml

src/qml/options_model.h
@@ -6,6 +6,8 @@
 #define BITCOIN_QML_OPTIONS_MODEL_H
 
 #include <common/settings.h>
+#include <common/system.h>
+#include <validation.h>
 
 #include <QObject>
 
@@ -19,6 +21,8 @@ class OptionsQmlModel : public QObject
     Q_OBJECT
     Q_PROPERTY(int dbcacheSizeMiB READ dbcacheSizeMiB WRITE setDbcacheSizeMiB NOTIFY dbcacheSizeMiBChanged)
     Q_PROPERTY(bool listen READ listen WRITE setListen NOTIFY listenChanged)
+    Q_PROPERTY(int maxScriptThreads READ maxScriptThreads CONSTANT)
+    Q_PROPERTY(int minScriptThreads READ minScriptThreads CONSTANT)
     Q_PROPERTY(bool natpmp READ natpmp WRITE setNatpmp NOTIFY natpmpChanged)
     Q_PROPERTY(bool prune READ prune WRITE setPrune NOTIFY pruneChanged)
     Q_PROPERTY(int pruneSizeGB READ pruneSizeGB WRITE setPruneSizeGB NOTIFY pruneSizeGBChanged)
@@ -33,6 +37,8 @@ public:
     void setDbcacheSizeMiB(int new_dbcache_size_mib);
     bool listen() const { return m_listen; }
     void setListen(bool new_listen);
+    int maxScriptThreads() const { return m_max_script_threads; }
+    int minScriptThreads() const { return m_min_script_threads; }
     bool natpmp() const { return m_natpmp; }
     void setNatpmp(bool new_natpmp);
     bool prune() const { return m_prune; }
@@ -62,6 +68,8 @@ private:
     // Properties that are exposed to QML.
     int m_dbcache_size_mib;
     bool m_listen;
+    const int m_max_script_threads{MAX_SCRIPTCHECK_THREADS};
+    const int m_min_script_threads{-GetNumCores()};
     bool m_natpmp;
     bool m_prune;
     int m_prune_size_gb;