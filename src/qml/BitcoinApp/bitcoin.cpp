5b77d3c197b7f95b426c5a86de0331e5c3c00abf
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-06-08 17:46:23 +0100

    qml: Add message box for InitError calls
    Github-Pull: bitcoin-core/gui-qml#17
    Rebased-From: 5e848d589560bf6c4caa1bf1e614a41f103bc1ff

--- src/qml/BitcoinApp/bitcoin.cpp
+++ src/qml/BitcoinApp/bitcoin.cpp
@@ -35,6 +35,18 @@ void SetupUIArgs(ArgsManager& argsman)
     argsman.AddArg("-resetguisettings", "Reset all settings changed in the GUI", ArgsManager::ALLOW_ANY, OptionsCategory::GUI);
     argsman.AddArg("-splash", strprintf("Show splash screen on startup (default: %u)", DEFAULT_SPLASHSCREEN), ArgsManager::ALLOW_ANY, OptionsCategory::GUI);
 }
+
+bool InitErrorMessageBox(
+    const bilingual_str& message,
+    [[maybe_unused]] const std::string& caption,
+    [[maybe_unused]] unsigned int style)
+{
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("message", QString::fromStdString(message.translated));
+    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/BitcoinApp/initerrormessage.qml")));
+    qGuiApp->exec();
+    return false;
+}
 } // namespace
 
 
@@ -46,6 +58,8 @@ int QmlGuiMain(int argc, char* argv[])
 #endif
 
     QGuiApplication app(argc, argv);
+
+    auto handler_message_box = ::uiInterface.ThreadSafeMessageBox_connect(InitErrorMessageBox);
 
     // Parse command-line options. We do this after qt in order to show an error if there are problems parsing these.
     SetupServerArgs(gArgs);
@@ -74,6 +88,8 @@ int QmlGuiMain(int argc, char* argv[])
     std::unique_ptr<interfaces::Init> init = interfaces::MakeGuiInit(argc, argv);
     std::unique_ptr<interfaces::Node> node = init->makeNode();
     node->baseInitialize();
+
+    handler_message_box.disconnect();
 
     NodeModel node_model;
     InitExecutor init_executor{*node};
