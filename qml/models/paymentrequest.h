2474095e5c7c5834153421d527a8510072b62c17
epicleafies <lukelpollard@gmail.com>
2026-04-23 18:19:47 -0400

    qt, model: Extend PaymentRequest with QR payload and metadata
    Add several properties to PaymentRequest that the upcoming detail
    page needs: addressType (derived from the destination variant),
    noteSelf (private note), qrPayload (reactive BIP21 URI that updates
    when amount, address, label, or message change), createdIso
    (ISO 8601 creation timestamp), and hasPaymentInfo (true when any of
    label, message, or amount is set).

--- qml/models/paymentrequest.h
+++ qml/models/paymentrequest.h
@@ -9,6 +9,7 @@
 
 #include <addresstype.h>
 
+#include <QDateTime>
 #include <QObject>
 #include <QString>
 
@@ -19,21 +20,30 @@ class PaymentRequest : public QObject
     Q_PROPERTY(QString addressFormatted READ addressFormatted NOTIFY addressChanged)
     Q_PROPERTY(QString label READ label WRITE setLabel NOTIFY labelChanged)
     Q_PROPERTY(QString message READ message WRITE setMessage NOTIFY messageChanged)
+    Q_PROPERTY(QString noteSelf READ noteSelf WRITE setNoteSelf NOTIFY noteSelfChanged)
     Q_PROPERTY(BitcoinAmount* amount READ amount CONSTANT)
+    Q_PROPERTY(QString addressType READ addressType NOTIFY addressChanged)
     Q_PROPERTY(QString amountError READ amountError NOTIFY amountErrorChanged)
     Q_PROPERTY(QString id READ id NOTIFY idChanged)
+    Q_PROPERTY(QString qrPayload READ qrPayload NOTIFY qrPayloadChanged)
+    Q_PROPERTY(QString createdIso READ createdIso NOTIFY createdIsoChanged)
+    Q_PROPERTY(bool hasPaymentInfo READ hasPaymentInfo NOTIFY qrPayloadChanged)
 
 public:
     explicit PaymentRequest(QObject* parent = nullptr);
 
     QString address() const;
     QString addressFormatted() const;
+    QString addressType() const;
 
     QString label() const;
     void setLabel(const QString& label);
 
     QString message() const;
     void setMessage(const QString& message);
+
+    QString noteSelf() const;
+    void setNoteSelf(const QString& note);
 
     BitcoinAmount* amount() const;
     QString amountError() const;
@@ -45,14 +55,23 @@ public:
     void setDestination(const CTxDestination& destination);
     CTxDestination destination() const;
 
+    QString qrPayload() const;
+
+    QString createdIso() const;
+    void setCreated(const QDateTime& dt);
+    bool hasPaymentInfo() const;
+
     Q_INVOKABLE void clear();
 
 Q_SIGNALS:
     void addressChanged();
     void labelChanged();
     void messageChanged();
+    void noteSelfChanged();
     void amountErrorChanged();
     void idChanged();
+    void qrPayloadChanged();
+    void createdIsoChanged();
 
 private:
     static QString FormatAddress(const QString& address);
@@ -60,9 +79,11 @@ private:
     CTxDestination m_destination;
     QString m_label;
     QString m_message;
+    QString m_noteSelf;
     QString m_amountError;
     BitcoinAmount* m_amount;
     QString m_id;
+    QDateTime m_created;
 };
 
 #endif // BITCOIN_QML_MODELS_PAYMENTREQUEST_H
