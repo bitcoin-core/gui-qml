a68d05b8980ea232578f6c4e32e146c44f02ca5e
epicleafies <lukelpollard@gmail.com>
2026-04-23 18:18:34 -0400

    refactor: Extract receive request entry types to dedicated header
    Move QmlReceiveRequestRecipient and QmlRecentRequestEntry from an
    anonymous namespace in walletqmlmodel.cpp into a dedicated header so
    they can be shared with the upcoming ReceiveRequestHistoryModel.
    
    Bump the recipient serialization version to 2, adding an optional
    noteSelf field, and widen the date serialization type to int64_t to
    avoid Y2038 truncation.

--- qml/models/walletqmlmodel.cpp
+++ qml/models/walletqmlmodel.cpp
@@ -8,6 +8,7 @@
 #include <qml/bitcoinamount.h>
 #include <qml/models/activitylistmodel.h>
 #include <qml/models/paymentrequest.h>
+#include <qml/models/receiverequestentry.h>
 #include <qml/models/sendrecipient.h>
 #include <qml/models/sendrecipientslistmodel.h>
 #include <qml/models/walletqmlmodeltransaction.h>
@@ -212,40 +213,6 @@ std::optional<std::vector<wallet::CRecipient>> BuildRecipients(const SendRecipie
 
     return vec_send;
 }
-
-struct QmlReceiveRequestRecipient
-{
-    static constexpr int CURRENT_VERSION{1};
-    int nVersion{CURRENT_VERSION};
-    std::string address;
-    std::string label;
-    CAmount amount{0};
-    std::string message;
-    std::string sPaymentRequest;
-    std::string authenticatedMerchant;
-
-    SERIALIZE_METHODS(QmlReceiveRequestRecipient, obj)
-    {
-        READWRITE(obj.nVersion, obj.address, obj.label, obj.amount, obj.message, obj.sPaymentRequest, obj.authenticatedMerchant);
-    }
-};
-
-struct QmlRecentRequestEntry
-{
-    static constexpr int CURRENT_VERSION{1};
-    int nVersion{CURRENT_VERSION};
-    int64_t id{0};
-    QDateTime date;
-    QmlReceiveRequestRecipient recipient;
-
-    SERIALIZE_METHODS(QmlRecentRequestEntry, obj)
-    {
-        unsigned int date_timet;
-        SER_WRITE(obj, date_timet = obj.date.toSecsSinceEpoch());
-        READWRITE(obj.nVersion, obj.id, date_timet, obj.recipient);
-        SER_READ(obj, obj.date = QDateTime::fromSecsSinceEpoch(date_timet));
-    }
-};
 } // namespace
 
 WalletQmlModel::WalletQmlModel(std::unique_ptr<interfaces::Wallet> wallet, QObject *parent)
