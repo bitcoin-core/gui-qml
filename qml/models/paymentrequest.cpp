65de6d7b3b106bef6f872b8297da5b8ec2260311
epicleafies <lukelpollard@gmail.com>
2026-04-23 18:19:47 -0400

    qt, model: Extend PaymentRequest with QR payload and metadata
    Add several properties to PaymentRequest that the upcoming detail
    page needs: addressType (derived from the destination variant),
    noteSelf (private note), qrPayload (reactive BIP21 URI that updates
    when amount, address, label, or message change), createdIso
    (ISO 8601 creation timestamp), and hasPaymentInfo (true when any of
    label, message, or amount is set).

--- qml/models/paymentrequest.cpp
+++ qml/models/paymentrequest.cpp
@@ -4,6 +4,8 @@
 
 #include <qml/models/paymentrequest.h>
 
+#include <qml/models/receiverequesthistorymodel.h>
+
 #include <key_io.h>
 
 #include <QString>
@@ -12,6 +14,10 @@ PaymentRequest::PaymentRequest(QObject* parent)
     : QObject(parent)
 {
     m_amount = new BitcoinAmount(this);
+    connect(m_amount, &BitcoinAmount::amountChanged, this, &PaymentRequest::qrPayloadChanged);
+    connect(this, &PaymentRequest::addressChanged, this, &PaymentRequest::qrPayloadChanged);
+    connect(this, &PaymentRequest::labelChanged, this, &PaymentRequest::qrPayloadChanged);
+    connect(this, &PaymentRequest::messageChanged, this, &PaymentRequest::qrPayloadChanged);
 }
 
 QString PaymentRequest::address() const
@@ -22,6 +28,24 @@ QString PaymentRequest::address() const
 QString PaymentRequest::addressFormatted() const
 {
     return FormatAddress(address());
+}
+
+QString PaymentRequest::addressType() const
+{
+    return std::visit([](const auto& dest) -> QString {
+        using T = std::decay_t<decltype(dest)>;
+        if constexpr (std::is_same_v<T, PKHash>) {
+            return QStringLiteral("Legacy");
+        } else if constexpr (std::is_same_v<T, ScriptHash>) {
+            return QStringLiteral("P2SH-SegWit");
+        } else if constexpr (std::is_same_v<T, WitnessV0KeyHash> || std::is_same_v<T, WitnessV0ScriptHash>) {
+            return QStringLiteral("Bech32");
+        } else if constexpr (std::is_same_v<T, WitnessV1Taproot>) {
+            return QStringLiteral("Bech32m");
+        } else {
+            return {};
+        }
+    }, m_destination);
 }
 
 QString PaymentRequest::label() const
@@ -50,6 +74,20 @@ void PaymentRequest::setMessage(const QString& message)
     }
     m_message = message;
     Q_EMIT messageChanged();
+}
+
+QString PaymentRequest::noteSelf() const
+{
+    return m_noteSelf;
+}
+
+void PaymentRequest::setNoteSelf(const QString& note)
+{
+    if (m_noteSelf == note) {
+        return;
+    }
+    m_noteSelf = note;
+    Q_EMIT noteSelfChanged();
 }
 
 BitcoinAmount* PaymentRequest::amount() const
@@ -97,19 +135,47 @@ CTxDestination PaymentRequest::destination() const
     return m_destination;
 }
 
+QString PaymentRequest::qrPayload() const
+{
+    const QString addr = address();
+    if (addr.isEmpty()) return {};
+    return ReceiveRequestHistoryModel::BuildBitcoinUri(addr, m_amount->satoshi(), m_label, m_message);
+}
+
+QString PaymentRequest::createdIso() const
+{
+    return m_created.isValid() ? m_created.toString(Qt::ISODate) : QString();
+}
+
+void PaymentRequest::setCreated(const QDateTime& dt)
+{
+    if (m_created == dt) return;
+    m_created = dt;
+    Q_EMIT createdIsoChanged();
+}
+
+bool PaymentRequest::hasPaymentInfo() const
+{
+    return !m_label.isEmpty() || !m_message.isEmpty() || m_amount->satoshi() > 0;
+}
+
 void PaymentRequest::clear()
 {
     m_destination = CNoDestination();
     m_label.clear();
     m_message.clear();
+    m_noteSelf.clear();
     m_amount->clear();
     m_amountError.clear();
     m_id.clear();
+    m_created = QDateTime();
     Q_EMIT addressChanged();
     Q_EMIT labelChanged();
     Q_EMIT messageChanged();
+    Q_EMIT noteSelfChanged();
     Q_EMIT amountErrorChanged();
     Q_EMIT idChanged();
+    Q_EMIT createdIsoChanged();
 }
 
 QString PaymentRequest::FormatAddress(const QString& address)
