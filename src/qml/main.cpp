f9b110d961a285da13c895551a0bcee6f9f5d9a6
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-06-08 17:19:00 +0100

    cmake: Add `bitcoin-qml` executable
    Github-Pull: bitcoin-core/gui-qml#5
    Rebased-From: cc7f2542a4a2e0bd80e1ca0ff25151280860d0cf

---
+++ src/qml/main.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2018-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qml/bitcoin.h>
+
+#include <compat/compat.h>
+#include <util/translation.h>
+
+#include <QCoreApplication>
+
+#include <functional>
+#include <string>
+
+/** Translate string to current locale using Qt. */
+extern const TranslateFn G_TRANSLATION_FUN = [](const char* psz) {
+    return QCoreApplication::translate("bitcoin-core", psz).toStdString();
+};
+
+const std::function<std::string()> G_TEST_GET_FULL_NAME{};
+
+MAIN_FUNCTION
+{
+    return QmlGuiMain(argc, argv);
+}
