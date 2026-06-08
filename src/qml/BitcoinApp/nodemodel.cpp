d2ab694778204fcecde7e029e6fbf337f813e225
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-06-08 16:31:29 +0100

    qml: Connect NodeModel to the NotifyBlockTip core signal
    Github-Pull: bitcoin-core/gui-qml#31
    Rebased-From: 3ead757f60434933a915b0c726e58b419ea8b8c4

--- src/qml/BitcoinApp/nodemodel.cpp
+++ src/qml/BitcoinApp/nodemodel.cpp
@@ -5,10 +5,12 @@
 #include <qml/BitcoinApp/nodemodel.h>
 
 #include <interfaces/node.h>
+#include <validation.h>
 
 NodeModel::NodeModel(interfaces::Node& node)
     : m_node{node}
 {
+    ConnectToBlockTipSignal();
 }
 
 void NodeModel::setBlockTipHeight(int new_height)
@@ -27,4 +29,19 @@ void NodeModel::startNodeInitializionThread()
 void NodeModel::startNodeShutdown()
 {
     Q_EMIT requestedShutdown();
+}
+
+void NodeModel::initializeResult([[maybe_unused]] bool success, interfaces::BlockAndHeaderTipInfo tip_info)
+{
+    // TODO: Handle the `success` parameter,
+    setBlockTipHeight(tip_info.block_height);
+}
+
+void NodeModel::ConnectToBlockTipSignal()
+{
+    assert(!m_handler_notify_block_tip);
+    m_handler_notify_block_tip = m_node.handleNotifyBlockTip(
+        [this](SynchronizationState state, interfaces::BlockTip tip, double verification_progress) {
+            setBlockTipHeight(tip.block_height);
+        });
 }
