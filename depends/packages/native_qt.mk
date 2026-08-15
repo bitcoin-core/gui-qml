d1dc8ee583ea263940d24ac6f56a0579a6db9073
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-08-15 02:54:10 +0000

    depends: Add Qt QML and Qt Quick modules
    Build qtdeclarative and qtshadertools for the target and host.
    Keep qlalr available and link Xau for static XCB builds.

--- depends/packages/native_qt.mk
+++ depends/packages/native_qt.mk
@@ -12,13 +12,21 @@ $(package)_patches += qttools_skip_dependencies.patch
 $(package)_patches += fix-macos26-qyield.patch
 $(package)_patches += fix_missed_headers.patch
 
+$(package)_qtshadertools_file_name=$(qt_details_qtshadertools_file_name)
+$(package)_qtshadertools_sha256_hash=$(qt_details_qtshadertools_sha256_hash)
+
+$(package)_qtdeclarative_file_name=$(qt_details_qtdeclarative_file_name)
+$(package)_qtdeclarative_sha256_hash=$(qt_details_qtdeclarative_sha256_hash)
+
 $(package)_qttranslations_file_name=$(qt_details_qttranslations_file_name)
 $(package)_qttranslations_sha256_hash=$(qt_details_qttranslations_sha256_hash)
 
 $(package)_qttools_file_name=$(qt_details_qttools_file_name)
 $(package)_qttools_sha256_hash=$(qt_details_qttools_sha256_hash)
 
-$(package)_extra_sources := $($(package)_qttranslations_file_name)
+$(package)_extra_sources := $($(package)_qtshadertools_file_name)
+$(package)_extra_sources += $($(package)_qtdeclarative_file_name)
+$(package)_extra_sources += $($(package)_qttranslations_file_name)
 $(package)_extra_sources += $($(package)_qttools_file_name)
 
 $(package)_top_download_path=$(qt_details_top_download_path)
@@ -52,14 +60,13 @@ $(package)_config_opts += -no-feature-concurrent
 $(package)_config_opts += -no-feature-network
 $(package)_config_opts += -no-feature-printsupport
 $(package)_config_opts += -no-feature-sql
-$(package)_config_opts += -no-feature-testlib
 $(package)_config_opts += -no-feature-xml
-$(package)_config_opts += -no-gui
 $(package)_config_opts += -no-widgets
 
 $(package)_config_opts += -no-glib
 $(package)_config_opts += -no-icu
 $(package)_config_opts += -no-libudev
+$(package)_config_opts += -no-opengl
 $(package)_config_opts += -no-openssl
 $(package)_config_opts += -no-zstd
 $(package)_config_opts += -qt-pcre
@@ -68,6 +75,7 @@ $(package)_config_opts += -no-feature-backtrace
 $(package)_config_opts += -no-feature-permissions
 $(package)_config_opts += -no-feature-process
 $(package)_config_opts += -no-feature-settings
+$(package)_config_opts += -no-feature-vulkan
 
 # Core tools.
 $(package)_config_opts += -no-feature-androiddeployqt
@@ -108,6 +116,8 @@ endef
 
 define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
+$(call fetch_file,$(package),$($(package)_download_path),$($(package)_qtshadertools_file_name),$($(package)_qtshadertools_file_name),$($(package)_qtshadertools_sha256_hash)) && \
+$(call fetch_file,$(package),$($(package)_download_path),$($(package)_qtdeclarative_file_name),$($(package)_qtdeclarative_file_name),$($(package)_qtdeclarative_sha256_hash)) && \
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_qttranslations_file_name),$($(package)_qttranslations_file_name),$($(package)_qttranslations_sha256_hash)) && \
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_qttools_file_name),$($(package)_qttools_file_name),$($(package)_qttools_sha256_hash)) && \
 $(call fetch_file,$(package),$($(package)_top_download_path),$($(package)_top_cmakelists_download_file),$($(package)_top_cmakelists_file_name)-$($(package)_version),$($(package)_top_cmakelists_sha256_hash)) && \
@@ -118,6 +128,8 @@ endef
 define $(package)_extract_cmds
   mkdir -p $($(package)_extract_dir) && \
   echo "$($(package)_sha256_hash)  $($(package)_source)" > $($(package)_extract_dir)/.$($(package)_file_name).hash && \
+  echo "$($(package)_qtshadertools_sha256_hash)  $($(package)_source_dir)/$($(package)_qtshadertools_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
+  echo "$($(package)_qtdeclarative_sha256_hash)  $($(package)_source_dir)/$($(package)_qtdeclarative_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_qttranslations_sha256_hash)  $($(package)_source_dir)/$($(package)_qttranslations_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_qttools_sha256_hash)  $($(package)_source_dir)/$($(package)_qttools_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_top_cmakelists_sha256_hash)  $($(package)_source_dir)/$($(package)_top_cmakelists_file_name)-$($(package)_version)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
@@ -126,6 +138,10 @@ define $(package)_extract_cmds
   $(build_SHA256SUM) -c $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   mkdir -p qtbase && \
   $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source) -C qtbase && \
+  mkdir -p qtshadertools && \
+  $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qtshadertools_file_name) -C qtshadertools && \
+  mkdir -p qtdeclarative && \
+  $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qtdeclarative_file_name) -C qtdeclarative && \
   mkdir -p qttranslations && \
   $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttranslations_file_name) -C qttranslations && \
   mkdir -p qttools && \
